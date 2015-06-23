281626f387f9e72720a978790bcb2182f922b20b
Sam Elkhateeb <sam.elkhateeb@egov.com>
2015-06-23 10:16:40 -0500

    Remove angularjs templates

--- src/main/resources/static/modules/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('UnikittyApp', [])
-
-.controller('home', function($scope, $http) {
-    var self = this;
-    $http.get('/resource/').success(function(data) {
-        self.greeting = data;
-    })
-    return self;
-});
--- src/main/resources/static/modules/app/app_test.js
+++
@@ -1,9 +0,0 @@
-describe('Application Module', function(){
-    it('should exist', function(){
-        try{
-            angular.module('UnikittyApp');
-        } catch(ex){
-            fail('Cannot load UnikittyApp  module');
-        }
-    });
-});

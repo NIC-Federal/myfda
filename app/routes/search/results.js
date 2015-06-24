8d2da03809d0b07d65d89e527bda802e77d122ba
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-24 11:21:49 -0500

    add animation to search results

--- app/routes/search/results.js
+++ app/routes/search/results.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    setupController: function(controller, model){
+        this._super(controller, model);
+
+       Ember.run.schedule('afterRender', this, function () {
+
+         let duration = 1500;
+         // Animations
+         $(".drug").velocity("transition.slideRightIn", {duration: duration, stagger: 150});
+       });
+     }
 });

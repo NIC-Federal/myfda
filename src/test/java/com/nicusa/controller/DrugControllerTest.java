63bc7ae0a1bd702a73ddd186af2ce8e137d818d2
Matt <mrankin@egov.com>
2015-06-24 17:26:57 -0500

    DrugController Unit Test Added

--- src/test/java/com/nicusa/controller/DrugControllerTest.java
+++ src/test/java/com/nicusa/controller/DrugControllerTest.java
@@ -1,5 +1,7 @@
 package com.nicusa.controller;
 
+import com.nicusa.util.HttpSlurper;
+
 import com.nicusa.assembler.DrugAssembler;
 import com.nicusa.domain.Drug;
 import com.nicusa.resource.DrugResource;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashSet;
 import javax.persistence.EntityManager;
 
 import static org.hamcrest.Matchers.is;
@@ -46,7 +50,7 @@ public class DrugControllerTest {
     assertThat(HttpStatus.OK, is(drugResourceResponseEntity.getStatusCode()));
     assertThat(drugResourceResponseEntity.getBody(), is(drugResource));
   }
-  
+
 
   @Test
   public void testGetDrugNotFound() {
@@ -71,12 +75,12 @@ public class DrugControllerTest {
     final String res = "[{\"value\":\"the result\"}]";
     drug.rest = mock(RestTemplate.class);
     when(drug.rest.getForObject(anyString(), (Class<?>) any(Class.class))).thenAnswer(
-      new Answer<String>() {
-        @Override
-        public String answer(InvocationOnMock inv) throws Throwable {
-          return res;
-        }
-      });
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return res;
+      }
+    });
     assertThat(drug.autocomplete("blah"), is("[\"the result\"]"));
   }
 
@@ -86,15 +90,105 @@ public class DrugControllerTest {
     final String res = "[{\"term\":\"abcdefg\"}]";
     drug.rest = mock( RestTemplate.class );
     when( drug.rest.getForObject(anyString(),(Class<?>)any(Class.class))).thenAnswer(
-        new Answer<String>() {
-          @Override
-          public String answer(InvocationOnMock inv) throws Throwable {
-            return res;
-          }
-        });
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return res;
+      }
+    });
     Set<String> uniis = drug.getUniisByName( "blah" );
     assertEquals( uniis.size(), 1 );
     assertTrue( uniis.contains( "abcdefg" ));
+  }
+
+  @Test
+  public void testGetBrandNamesByNameAndUniis () throws IOException {
+    DrugController drug = new DrugController();
+    final String s = "{\"meta\":{\"disclaimer\":\"openFDA is a beta research project and not for clinical use. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.\",\"license\":\"http://open.fda.gov/license\",\"last_updated\":\"2015-05-31\"},\"results\":[{\"term\":\"ADVIL PM\",\"count\":2}]}";
+    drug.slurp = mock( HttpSlurper.class );
+    when( drug.slurp.getData(anyString())).thenAnswer(
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return s;
+      }
+    });
+    Set<String> uniis = new HashSet<String>();
+    uniis.add("8GTS82S83M");
+    uniis.add("WK2XYI10QM");
+    Map<String,Set<String>> result = drug.getBrandNamesByNameAndUniis( "blah", uniis );
+    assertTrue( result != null );
+  }
+
+  @Test
+  public void testGetBrandNamesByNameAndUnii () throws IOException {
+    DrugController drug = new DrugController();
+    final String s = "{\"meta\":{\"disclaimer\":\"openFDA is a beta research project and not for clinical use. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.\",\"license\":\"http://open.fda.gov/license\",\"last_updated\":\"2015-05-31\"},\"results\":[{\"term\":\"ADVIL PM\",\"count\":2}]}";
+    drug.slurp = mock( HttpSlurper.class );
+    when( drug.slurp.getData(anyString())).thenAnswer(
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return s;
+      }
+    });
+
+    // Set<String> uniis = new HashSet<String>();
+    // uniis.add("8GTS82S83M");
+    // uniis.add("WK2XYI10QM");
+
+    Set<String> result = drug.getBrandNamesByNameAndUnii( "blah", "8GTS82S83M" );
+    assertTrue( result.contains("ADVIL PM") );
+  }
+
+  @Test
+  public void testGetGenericNameByUnii () throws IOException {
+    DrugController drug = new DrugController();
+    final String s = "{\"meta\":{\"disclaimer\":\"openFDA is a beta research project and not for clinical use. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.\",\"license\":\"http://open.fda.gov/license\",\"last_updated\":\"2015-05-31\"},\"results\":[{\"term\":\"DIPHENHYDRAMINE HYDROCHLORIDE\",\"count\":245}]}";
+    drug.rest = mock( RestTemplate.class );
+    when( drug.rest.getForObject(anyString(),(Class<?>)any(Class.class))).thenAnswer(
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return s;
+      }
+    });
+    String resultunii = drug.getGenericNameByUnii( "blah" );
+    assertTrue( resultunii != null );
+  }
+
+  @Test
+  public void testGetRxcuiByBrandName () throws IOException {
+    DrugController drug = new DrugController();
+    final String s = "{\"idGroup\":{\"name\":\"advil\",\"rxnormId\":[\"153010\"]}}";
+    drug.slurp = mock( HttpSlurper.class );
+    when( drug.slurp.getData(anyString())).thenAnswer(
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return s;
+      }
+    });
+    Long resultRxcuis = drug.getRxcuiByBrandName( "blah" );
+    assertTrue( resultRxcuis != null );
+  }
+
+  @Test
+  public void testGetActiveIngrediantsByRxcui () throws IOException {
+    DrugController drug = new DrugController();
+    final String s = "{\"relatedGroup\":{\"rxcui\":\"643061\",\"rela\":[\"tradename_of\",\"has_precise_ingredient\"],\"conceptGroup\":[{\"tty\":\"IN\",\"conceptProperties\":[{\"rxcui\":\"3498\",\"name\":\"Diphenhydramine\",\"synonym\":\"\",\"tty\":\"IN\",\"language\":\"ENG\",\""
+    +"suppress\":\"N\",\"umlscui\":\"C0012522\"},{\"rxcui\":\"5640\",\"name\":\"Ibuprofen\",\"synonym\":\"\",\"tty\":\"IN\",\"language\":\"ENG\",\"suppress\":\"N\",\"umlscui\":\"C0020740\"}]},{\"tty\":\"PIN\",\"conceptProperties\":[{\"rxcui\":\"1362\",\"name\":\"Diphenhydramine Hydrochloride\""
+    +",\"synonym\":\"\",\"tty\":\"PIN\",\"language\":\"ENG\",\"suppress\":\"N\",\"umlscui\":\"C0004963\"},{\"rxcui\":\"82004\",\"name\":\"Diphenhydramine Citrate\",\"synonym\":\"\",\"tty\":\"PIN\",\"language\":\"ENG\",\"suppress\":\"N\",\"umlscui\":\"C0282144\"}]}]}}";
+    drug.slurp = mock( HttpSlurper.class );
+    when( drug.slurp.getData(anyString())).thenAnswer(
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return s;
+      }
+    });
+    Set<String> result = drug.getActiveIngrediantsByRxcui( new Long(643061) );
+    assertTrue( result != null );
   }
 
   @Test
@@ -103,13 +197,13 @@ public class DrugControllerTest {
     final String res = "[{\"value\":\"the result\"}]";
     drug.rest = mock( RestTemplate.class );
     when( drug.rest.getForObject(anyString(),(Class<?>)any(Class.class))).thenAnswer(
-       new Answer<String>() {
-         @Override
-         public String answer(InvocationOnMock inv) throws Throwable {
-           return res;
-         }
-       });
-    //assertThat( drug.search( "blah", 10, 0 ), is( res ));
+    new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock inv) throws Throwable {
+        return res;
+      }
+    });
+    // assertThat( drug.search( "blah", 10, 0 ), is( res ));
     //TODO FIX THIS
   }
 }

4b80607875a4503f8d46ac6254fb2c38329f4984
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-24 17:31:13 -0500

    adding comment

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    // This deals with determining whether or not to display the navigation search bar
     isDashboard: false,
     showNavSearch: function() {
         var currentPath = this.get("currentPath");
         var isDashboard = currentPath ? currentPath.indexOf("dashboard") === 0 : false;
         this.set("isDashboard", isDashboard);
     }.observes('currentPath').on("init")
+
 });

124f7c729f1b7b23584372b535c663cab3913d3b
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-24 17:28:57 -0500

    setup search components to perform search and redirect to search page

--- app/router.js
+++ app/router.js
@@ -9,11 +9,11 @@ Router.map(function() {
   this.route('dashboard', {path: "/"});
 
   this.route('drugs', function() {
-    this.route('drug');
+    this.route('drug' {path: "/drug/:drug_id"});
   });
 
   this.route('search', function() {
-    this.route('results');
+    this.route('results', {path: ':keyword'});
   });
   this.route('my-meds', function() {
     this.route('notifications');

e3969a471b470340b7fea109aa6c706babdc9c5c
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-25 11:02:01 -0500

    cleanup some unused code

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        //return this.store.find("drug/recalls");
         return $.getJSON("drug/recalls");
     },
     setupController: function(controller, model){
@@ -12,7 +11,6 @@ export default Ember.Route.extend({
 
          let duration = 1500;
          // Animations
-         //$("#dashboard-search").velocity("transition.slideRightIn", {duration: duration, display: "table"});
          $(".recall").velocity("transition.slideRightIn", {duration: duration, stagger: 150});
        });
    }

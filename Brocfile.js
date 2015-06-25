5ca99d7fd595f94fff5f9e34419e00c667450479
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-25 11:01:42 -0500

    Moment.js integration for formatting recall dates

--- Brocfile.js
+++ Brocfile.js
@@ -18,6 +18,7 @@ app.import("bower_components/velocity/velocity.js");
 app.import("bower_components/velocity/velocity.ui.js");
 app.import('bower_components/us-map/lib/raphael.js');
 app.import('bower_components/us-map/jquery.usmap.js');
+app.import('bower_components/moment/moment.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.

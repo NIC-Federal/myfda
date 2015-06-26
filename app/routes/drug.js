f6b71d6f88ab2b6ffdfb513eeb6cdc2701a372ea
Matt <mrankin@egov.com>
2015-06-26 16:30:36 -0500

    Drug effect male and female percentage api fetching added

--- app/routes/drug.js
+++ app/routes/drug.js
@@ -5,18 +5,36 @@ export default Ember.Route.extend({
   model: function(params) {
     return Ember.RSVP.hash({
       genderCount: $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:' + '"8GTS82S83M"' + '+AND+patient.reaction.reactionmeddrapt:%22' + 'headache' + '%22&count=patient.patientsex').then(function(data) {
-        // var node = Ember.Object.create({
-        //   data: {
-        //     term: 'test' + data.term,
-        //     count: 'count' + data.count
-        //   }
-        // });
-        // return JSON.stringify(node.get('data'));
-        return data;
+        var maleCount = 0;
+        var femaleCount = 0;
+        var total = 0;
+
+        $.each( data.results, function( key, value ) {
+          if(value.term === 1){ // Male
+            maleCount = value.count;
+          } else if(value.term === 2){ // Female
+            femaleCount = value.count;
+          }
+        });
+
+        total = maleCount + femaleCount;
+
+        var results = {
+          male: {
+              count: maleCount,
+              pct: Math.round((maleCount/total) * 100)
+          },
+          female: {
+              count: femaleCount,
+              pct: Math.round((femaleCount/total) * 100)
+          },
+          total: total
+        };
+        return results;
       }),
-      ages: $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:' + '"8GTS82S83M"' + '+AND+patient.reaction.reactionmeddrapt:%22' + 'headache' + '%22&count=patientonsetage').then(function(data) {
-        return data;
-      }),
+      // ages: $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:' + '"8GTS82S83M"' + '+AND+patient.reaction.reactionmeddrapt:%22' + 'headache' + '%22&count=patientonsetage').then(function(data) {
+      //   return data;
+      // }),
       effects: $.getJSON("event?unii=" + params.drug_id),
       recalls: $.getJSON("drug/enforcements?unii=" + params.drug_id)
     });

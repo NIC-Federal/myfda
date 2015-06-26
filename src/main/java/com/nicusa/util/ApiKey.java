e4da3fc9ecffb387cacf3fb9bae1142f1c54a2c1
rkuhns <solinari@netscape.net>
2015-06-26 12:57:40 -0600

    added guava and rate limited on api key returns

--- src/main/java/com/nicusa/util/ApiKey.java
+++ src/main/java/com/nicusa/util/ApiKey.java
@@ -1,16 +1,25 @@
 package com.nicusa.util;
 
+import com.google.common.util.concurrent.RateLimiter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApiKey {
+  private static final Logger log = LoggerFactory.getLogger(ApiKey.class);
+
+  // 4 requests per second per key
+  private static final RateLimiter rateLimiter = RateLimiter.create( 4.0 );
 
   @Autowired
   @Value("${api.fda.key}")
   String fdaApiKey;
 
   public String getFdaApiKey () {
+    rateLimiter.acquire(); // may wait
     return this.fdaApiKey;
   }
 

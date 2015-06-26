fbb7f20ba5cb0b2ab5b4fdb00efb690ac0ba6443
Chris Henn <chris.henn@egov.com>
2015-06-26 17:52:30 -0500

    Updated Ssl config to use external application.properties

--- src/main/java/com/nicusa/SslTomcatConnectionCustomizer.java
+++ src/main/java/com/nicusa/SslTomcatConnectionCustomizer.java
@@ -1,18 +1,25 @@
 package com.nicusa;
 
+import java.io.FileNotFoundException;
+
 import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.util.ResourceUtils;
 
 public class SslTomcatConnectionCustomizer implements TomcatConnectorCustomizer
 {
-    private String absoluteKeystoreFile;
+    private static final Logger log = LoggerFactory.getLogger(SslTomcatConnectionCustomizer.class);
+    
+    private String keystoreFile;
     private String keystorePassword;
     private String keystoreType;
     private String keystoreAlias;
 
     public SslTomcatConnectionCustomizer(String absoluteKeystoreFile, String keystorePassword, String keystoreType, String keystoreAlias)
     {
-        this.absoluteKeystoreFile = absoluteKeystoreFile;
+        this.keystoreFile = absoluteKeystoreFile;
         this.keystorePassword = keystorePassword;
         this.keystoreType = keystoreType;
         this.keystoreAlias = keystoreAlias;
@@ -21,22 +28,35 @@ public class SslTomcatConnectionCustomizer implements TomcatConnectorCustomizer
     @Override
     public void customize (Connector connector)
     {
-        if (absoluteKeystoreFile != null)
+        if (keystoreFile != null)
         {
-            connector.setPort(8443);
-            connector.setSecure(true);
-            connector.setScheme("https");
-
-            connector.setAttribute("SSLEnabled", true);
-            connector.setAttribute("sslProtocol", "TLS");
-            connector.setAttribute("protocol", "org.apache.coyote.http11.Http11Protocol");
-            connector.setAttribute("clientAuth", false);
-            connector.setAttribute("keystoreFile", absoluteKeystoreFile);
-            connector.setAttribute("keystoreType", keystoreType);
-            connector.setAttribute("keystorePass", keystorePassword);
-            connector.setAttribute("keystoreAlias", keystoreAlias);
-            connector.setAttribute("keyPass", keystorePassword);
-        }
+            String absoluteKeystoreFile = null;
+            try {
+                absoluteKeystoreFile = ResourceUtils.getFile(keystoreFile).getAbsolutePath();
+    
+                connector.setPort(8443);
+                connector.setSecure(true);
+                connector.setScheme("https");
+    
+                connector.setAttribute("SSLEnabled", true);
+                connector.setAttribute("sslProtocol", "TLS");
+                connector.setAttribute("protocol", "org.apache.coyote.http11.Http11Protocol");
+                connector.setAttribute("clientAuth", false);
+                connector.setAttribute("keystoreFile", absoluteKeystoreFile);
+                connector.setAttribute("keystoreType", keystoreType);
+                connector.setAttribute("keystorePass", keystorePassword);
+                connector.setAttribute("keystoreAlias", keystoreAlias);
+                connector.setAttribute("keyPass", keystorePassword);
+            } catch (FileNotFoundException fnfe)
+            {
+                log.error("Could not find keystoreFile:  "+keystoreFile+".");
+            }
+            
+            if(absoluteKeystoreFile == null)
+            {
+                log.warn("Starting server with SSL encryption turned off");
+            }
+        }   
     }
 
 }
--- src/main/java/com/nicusa/UiApplication.java
+++ src/main/java/com/nicusa/UiApplication.java
@@ -1,5 +1,6 @@
 package com.nicusa;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +9,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -20,8 +22,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,46 +32,53 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@PropertySources({
-  @PropertySource(value = "file:${sys:user.home}/.nic/unikitty.properties", ignoreResourceNotFound = true),
-  @PropertySource(value = "file:${user.home}/.nic/unikitty.properties", ignoreResourceNotFound = true) })
 public class UiApplication {
-
+    
   private static final Logger log = LoggerFactory.getLogger(UiApplication.class);
 
-  private static final String KEYSTORE_FILE = "keystoreFile";
-  private static final String KEYSTORE_ALIAS = "keystoreAlias";
-  private static final String KEYSTORE_PASSWORD = "keyPwd";
-
-  @Autowired
-  Environment env;
-
-
+  @Value("${keystore.file:}")
+  private String keystoreFile;
+  
+  @Value("${keystore.pass:}")
+  private String keystorePassword;
+  
+  @Value("${keystore.type:}")
+  private String keystoreType;
+  
+  @Value("${keystore.alias:}")
+  private String keystoreAlias;
+  
+  
   public static void main(String[] args) {
     SpringApplication.run(UiApplication.class, args);
   }
 
+  @RequestMapping("/resource")
+  public Map<String, Object> home() {
+    Map<String, Object> model = new HashMap<>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello World");
+    return model;
+  }
+  
   @Bean
   public EmbeddedServletContainerCustomizer containerCustomizer() throws FileNotFoundException
   {
       String absoluteKeystoreFile = null;
-      String keyfileAlias = null;
-      String keyfilePassword = null;
-
       try {
-          String absoluteKeystoreFileName = env.getProperty("absolute.file.keystore");
-          keyfileAlias = env.getProperty("keyfile.alias");
-          keyfilePassword = env.getProperty("keyfile.password");
-
-          absoluteKeystoreFile = ResourceUtils.getFile(absoluteKeystoreFileName).getAbsolutePath();
+          if(keystoreFile != null && keystoreFile.length() > 0)
+          {
+              log.info("Absolute keystore file: "+keystoreFile);
+              absoluteKeystoreFile = ResourceUtils.getFile(keystoreFile).getAbsolutePath();
+          }
       } catch (Exception e)
       {
           log.warn("Keystore not defined ");
       }
-
+  
 
       final TomcatConnectorCustomizer customizer = new SslTomcatConnectionCustomizer(
-          absoluteKeystoreFile, keyfilePassword, "PKCS12", keyfileAlias);
+              absoluteKeystoreFile, keystorePassword, keystoreType, keystoreAlias); 
 
       return new EmbeddedServletContainerCustomizer() {
 
@@ -85,11 +92,30 @@ public class UiApplication {
       };
   }
 
-  @Bean
-  public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-    mappingJackson2HttpMessageConverter.setPrettyPrint(true);
-    return mappingJackson2HttpMessageConverter;
+  @Configuration
+  @EnableWebSecurity
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  @PropertySources({ 
+    @PropertySource(value = "file:${sys:user.home}/.nic/unikitty.properties", ignoreResourceNotFound = true),
+    @PropertySource(value = "file:${user.home}/.nic/unikitty.properties", ignoreResourceNotFound = true) })
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.httpBasic().and().authorizeRequests()
+        .antMatchers("/","/index.html"
+                ,"/recalls"
+                ,"/drug/recalls"
+                ,"/drug/enforcements").permitAll()
+        .anyRequest().fullyAuthenticated()
+        .and().csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth
+        .inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER");
+    }
   }
 
 }

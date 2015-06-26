dc10f79c3b140f031c37715edb3f533a282017b2
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-25 21:32:33 -0500

    get search result query passing through to search results page

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     actions: {
         performSearch: function(keyword) {
+            var controller = this.get("controller");
+            this.refresh();
+            controller.set("searchQuery", keyword);
             this.transitionTo('search.results', keyword);
+        },
+        error: function() {
+            this.transitionTo('index');
         }
-    }
+    },
+
 });

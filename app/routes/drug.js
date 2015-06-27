8c57f0b38c5424bcd2dce683191519564c15f718
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-27 15:12:48 -0500

    clean up drug route and remove unused actions

--- app/routes/drug.js
+++ app/routes/drug.js
@@ -4,55 +4,15 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return Ember.RSVP.hash({
-
-      /////////////////////////
-      // Gender
-      ////////////////////////
-      genderCount: $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:' + params.drug_id + '+AND+patient.reaction.reactionmeddrapt:%22' + 'headache' + '%22&count=patient.patientsex').then(function(data) {
-        var maleCount = 0;
-        var femaleCount = 0;
-        var total = 0;
-
-        $.each( data.results, function( key, value ) {
-          if(value.term === 1){ // Male
-            maleCount = value.count;
-          } else if(value.term === 2){ // Female
-            femaleCount = value.count;
-          }
-        });
-
-        total = maleCount + femaleCount;
-
-        var results = {
-          male: {
-              count: maleCount,
-              pct: Math.round((maleCount/total) * 100)
-          },
-          female: {
-              count: femaleCount,
-              pct: Math.round((femaleCount/total) * 100)
-          },
-          total: total
-        };
-        return results;
-      }),
-
-      /////////////////////////
-      // Age
-      ////////////////////////
-//    ages: $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:' + '"8GTS82S83M"' + '+AND+patient.reaction.reactionmeddrapt:%22' + 'headache' + '%22&count=patientonsetage').then(function(data) {
-//      return data;
-//    }),
-
       /////////////////////////
       // Effects
       ////////////////////////
-      effects: $.getJSON("event?unii=" + params.drug_id),
+      effects: $.getJSON("http://localhost:8080/event?unii=" + params.drug_id),
 
       /////////////////////////
       // Recalls
       ////////////////////////
-      recalls: $.getJSON("drug/enforcements?unii=" + params.drug_id)
+      recalls: $.getJSON("http://localhost:8080/drug/enforcements?unii=" + params.drug_id)
     });
   },
   setupController: function(controller, model){
@@ -75,15 +35,5 @@ export default Ember.Route.extend({
 		$('[data-toggle="tooltip"]').tooltip();
 
     });
-  },
-
-  actions: {
-    loadEffectData: function(name) {
-      console.log("Route recieved: " + name);
-    //   $.getJSON('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.unii:WK2XYI10QM+AND+patient.reaction.reactionmeddrapt:%22' + name + '%22&count=patient.patientsex').then(function(response){
-    //     console.log(response);
-    //     $("#" + name).text(response.results[0].count);
-    //   });
-    }
   }
 });

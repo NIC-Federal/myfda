8d212a95312e968cd784606d871b87545a2f943f
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-27 15:19:38 -0500

    tests for all new helpers

---
+++ tests/unit/helpers/find-effect-count-test.js
@@ -0,0 +1,10 @@
+import { findEffectCount } from '../../../helpers/find-effect-count';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | find effect count');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = findEffectCount(42);
+  assert.ok(result);
+});

701e7e99882b9c7ffd486b10a94831386af8e8b5
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-06-27 15:11:52 -0500

    classnames on bootstrap accordions so they don’t conflict with ember view

--- app/components/effect-accordion.js
+++ app/components/effect-accordion.js
@@ -10,14 +10,15 @@ export default Ember.Component.extend({
   femalePercent: 0,
   ages: [],
   needs: ['application'],
+  // These classes allow bootstrap to declare only one open panel at a time
+  classNames: ['effect panel panel-default'],
   _initializeAccordion: function() {
   }.on('didInsertElement'),
   actions: {
     sendEffectData: function() {
-      // var drugId = this.get("drugId");
-      var name = this.get("name");
-      this.sendAction('action', name);
-
+      var drugId = this.get("drugId");
+      var drugName = this.get("name");
+      this.sendAction('action', drugName, drugId);
     }
   }
 

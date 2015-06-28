fff2658b079908f39e6d3c79f382625b4eb399d8
Patrick Iverson <hello@patrickiverson.com>
2015-06-28 17:12:13 -0500

    Remove percentage rounding.

--- app/routes/drug/effect.js
+++ app/routes/drug/effect.js
@@ -119,18 +119,18 @@ export default Ember.Route.extend({
                             break;
                         default:
                     }
-                    if(value.term > 0 && value.term < 5)
+                    if(value.term > 0 && value.term < 6)
                     {
                         total += value.count;
                     }
 
                 });
 
-                result.Outcome1.pct = Math.round((result.Outcome1.count / total)*100);
-                result.Outcome2.pct = Math.round((result.Outcome2.count / total)*100);
-                result.Outcome3.pct = Math.round((result.Outcome3.count / total)*100);
-                result.Outcome4.pct = Math.round((result.Outcome4.count / total)*100);
-                result.Outcome5.pct = Math.round((result.Outcome5.count / total)*100);
+                result.Outcome1.pct = (result.Outcome1.count / total)*100;
+                result.Outcome2.pct = (result.Outcome2.count / total)*100;
+                result.Outcome3.pct = (result.Outcome3.count / total)*100;
+                result.Outcome4.pct = (result.Outcome4.count / total)*100;
+                result.Outcome5.pct = (result.Outcome5.count / total)*100;
 
                 return result;
               })

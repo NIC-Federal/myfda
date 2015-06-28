1e9b363504c185de476583f39b7908d3ab4b9cd0
Patrick Iverson <hello@patrickiverson.com>
2015-06-28 17:12:36 -0500

    Fix JSON references.

--- app/routes/drug.js
+++ app/routes/drug.js
@@ -4,15 +4,9 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return Ember.RSVP.hash({
-      /////////////////////////
-      // Effects
-      ////////////////////////
-      effects: $.getJSON("http://localhost:8080/event?unii=" + params.drug_id),
+      effects: $.getJSON("event?unii=" + params.drug_id),
 
-      /////////////////////////
-      // Recalls
-      ////////////////////////
-      recalls: $.getJSON("http://localhost:8080/drug/enforcements?unii=" + params.drug_id)
+      recalls: $.getJSON("drug/enforcements?unii=" + params.drug_id)
     });
   },
   setupController: function(controller, model){
@@ -22,12 +16,13 @@ export default Ember.Route.extend({
 
 		let duration = 1500;
 
-		$('a[data-toggle="tab"]').on('shown.bs.tab', function () {
-			$(".effect", this).velocity("transition.slideRightIn", {duration: duration, stagger: 150});
+		$('#tab-effects').on('shown.bs.tab', function () {
+			$(".effect").velocity("transition.slideRightIn", {duration: duration, stagger: 150});
 		});
 
 		$('.collapse').on('show.bs.collapse', function(){
 			$(this).parent().find(".fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+			$(".panel-body").velocity("fadeIn", {duration: duration});
 		}).on('hide.bs.collapse', function(){
 			$(this).parent().find(".fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
 		});

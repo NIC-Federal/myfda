353fff94eeecb4622762c132eeacb59ce6354851
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-07-01 11:12:52 -0600

    Adverse effects search

--- app/controllers/drug.js
+++ app/controllers/drug.js
@@ -6,6 +6,7 @@ export default Ember.Controller.extend({
     UserData: Ember.computed.alias('controllers.application.content'),
     // Current Drug
     drugName: Ember.computed.alias('controllers.application.currentDrugName'),
+
     // Search Drug Interactions
     interactionSearchQuery: null,
     //interactionSearchQueryRegEx: new RegExp(interactionSearchQuery, 'i'),
@@ -23,5 +24,23 @@ export default Ember.Controller.extend({
         return this.model.interactions.interactions.filter(function(interaction){
             return interaction.toDrug.match(regex);
         });
-    }.property('interactionSearchQuery')
+    }.property('interactionSearchQuery'),
+
+    // Search Adverse Effects
+    effectSearchQuery: null,
+    effectSearchResults: function() {
+        // Get the search term
+        var searchTerm = this.get('effectSearchQuery');
+
+        // return out of function if blank
+        if (!searchTerm) {return;}
+
+        // Create a RegEx that ignores case
+        var regex = new RegExp(searchTerm, 'i');
+
+        // Return the results
+        return this.model.effects.effect.filter(function(effect){
+            return effect.effect.match(regex);
+        });
+    }.property('effectSearchQuery')
 });
